fe1b6bc1c0142bcdb5857b996e8d17543ff27e2c
Lucas Aguiar <lucasaguiar.floripa@gmail.com>
2021-07-12 13:59:05 -0300

    WebSite first commit.

--- README.md
+++
@@ -1,2 +0,0 @@
-# Robo-sinais-youtube
-Projeto criado em conjunto com a série de vídeos de como criar um robô para a iqoption para meu canal.
--- main.py
+++
@@ -1,6 +0,0 @@
-from iqoptionapi.stable_api import IQ_Option
-
-API = IQ_Option("wmieky.m@vlvstech.com", "12345678")
-API.connect()
-if (API.check_connect()) == True: print("Conectado!")
-else: print("Dados incorretos!")
